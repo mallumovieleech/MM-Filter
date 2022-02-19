374163e7e32d4c45805e47f8a5c9b3bf3ff82cca
mbrhdrg01 <96500032+mbrhdrg01@users.noreply.github.com>
2022-02-19 15:52:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -398,7 +398,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer()
     elif query.data == "start":
         buttons = [[
-            InlineKeyboardButton('👉𝚂𝙴𝙰𝚁𝙲𝙷 𝙼𝙾𝚅𝙸𝙴𝚂 & 𝚂𝙴𝚁𝙸𝙴𝚂 𝙵𝚁𝙾𝙼 𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿', url=f'https://t.me/+6MW3sRKDdQIyOWJl')
+            InlineKeyboardButton('👉𝚂𝙴𝙰𝚁𝙲𝙷 𝙼𝙾𝚅𝙸𝙴𝚂 & 𝚂𝙴𝚁𝙸𝙴𝚂 𝙵𝚁𝙾𝙼 𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿', url=f'https://t.me/+y2bkXyqYNeExYWRl')
             ],[
             InlineKeyboardButton('🔍 Search', url=f'https://t.me/+y2bkXyqYNeExYWRl'),
             InlineKeyboardButton('🤖 Updates', url=f'https://t.me/+y2bkXyqYNeExYWRl')
