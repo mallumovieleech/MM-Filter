b9c78a26d5ccc8520323062de9105d374f0e1d75
mallumovieleech <88672268+mallumovieleech@users.noreply.github.com>
2023-04-04 07:47:14 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/+UAfifamyqQgyMGFl')
+            InlineKeyboardButton('📢 Updates', url='https://t.me/+BmYsCpTjuG82Yjc0')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
